dfbedad53b783cddfd1663ed8d5d0fc92cb406b4
nishadisb <41675400+nishadisb@users.noreply.github.com>
2020-03-17 21:24:44 +0530

    Add files via upload

---
+++ mini/n1.php
@@ -0,0 +1,66 @@
+<html>
+<head>
+     <title>
+          Places in Sri Lanka
+     </title>
+     <link rel="stylesheet" type="text/css" href="css/css1.css">
+</head>
+<body>
+     <div class="wrpper">
+          <div class="top-br clearfix">
+          <div class="top-br-links">
+               
+          
+          
+          <ul>
+               <li><a href="#">Home</a></li>
+               <li><a href="#">Places</a></li>
+               <li><a href="#">Hotels</a></li>
+               <li><a href="#">about Us</a></li>
+
+          </ul>
+          </div><!--top-br-links-->
+          
+          <div class="site-search">
+               <form>
+                    <form method="post" action="index.php" >
+                         <input type="search" name="search">
+                         <button type="submit">Search</button>
+               </form>
+          </div><!--site_search-->
+
+          </div>
+          <!--top-br-->
+          <header class="clearfix">
+               <div class="logo">
+                    <h1>
+                         Come And See!Enjoy Your Life
+                    </h1>
+                    <p>This is your Choises</p>
+               </div><!-- logo -->
+               <div class="socialmedia">
+                    <ul>
+                       <li><a href="#"><img src="img/download (1).png"  width="30" height="30"></a></li>
+                       <li><a href="#"><img src="img/download (2).png"  width="30" height="30"></a></li>
+                       <li><a href="#"><img src="img/download (3).png"  width="30" height="30"></a></li>
+                       <li><a href="#"><img src="img/download (6).png"  width="30" height="30"></a></li>
+                    </ul>
+                    
+               </div><!-- socialmedia -->
+          </header>
+          <nav>
+               <ul>
+                 <li> <a href="#">HOME</li>
+                <li> <a href="#">ABOUT US</li>
+                <li> <a href="#">SERVICES</li>
+                <li> <a href="#">PLACES</li>
+                <li> <a href="#">CONTACT US</li>
+
+               </ul>
+          </nav>
+   </div><!-- wrpper -->
+
+</body>
+
+
+</html>
